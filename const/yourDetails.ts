74f33d7b833c297448e64bdd3c4844e1e43817da
luwisah <luwisah@gmail.com>
2024-05-18 00:45:22 +0100

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const myEditionDropContractAddress: string = "0xcf67980B402e6Dbb2F399DF7F12E83AbD761829D";
+export const myEditionDropContractAddress: string = "0x4962a52B595B52c1eda4666bF475291AFcc4f8aD";
 
 // Replace this with your token id
 export const tokenId: string = "0";
